9e26bf0dd8a920e7ca1456d04aaf981d22d9974e
nancyordor <chinyereordor@gmail.com>
2020-12-18 21:14:19 +0100

    heroku hosting details added

--- dbh.php
+++ dbh.php
@@ -1,9 +1,16 @@
 <?php
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'mainscaproject');
+// localhost database connection
+// define('DB_SERVER', 'localhost');
+// define('DB_USERNAME', 'root');
+// define('DB_PASSWORD', '');
+// define('DB_NAME', 'mainscaproject');
  
+// remote database connection
+define('DB_SERVER', 'remotemysql.com');
+define('DB_USERNAME', 'JH2gC9TryT');
+define('DB_PASSWORD', 'g17u6CQYFp');
+define('DB_NAME', 'mainscaproject');
+
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
  
